627b60ded70020d4ff1a19a87a9596549dd90236
AnhNDT <anhnguyen.lk@gmail.com>
2018-07-25 15:36:45 +0700

    init projects

---
+++ android/src/main/java/io/github/anhndt/nativeutils/flutternativeutils/FlutterNativeUtilsPlugin.java
@@ -0,0 +1,25 @@
+package io.github.anhndt.nativeutils.flutternativeutils;
+
+import io.flutter.plugin.common.MethodCall;
+import io.flutter.plugin.common.MethodChannel;
+import io.flutter.plugin.common.MethodChannel.MethodCallHandler;
+import io.flutter.plugin.common.MethodChannel.Result;
+import io.flutter.plugin.common.PluginRegistry.Registrar;
+
+/** FlutterNativeUtilsPlugin */
+public class FlutterNativeUtilsPlugin implements MethodCallHandler {
+  /** Plugin registration. */
+  public static void registerWith(Registrar registrar) {
+    final MethodChannel channel = new MethodChannel(registrar.messenger(), "flutter_native_utils");
+    channel.setMethodCallHandler(new FlutterNativeUtilsPlugin());
+  }
+
+  @Override
+  public void onMethodCall(MethodCall call, Result result) {
+    if (call.method.equals("getPlatformVersion")) {
+      result.success("Android " + android.os.Build.VERSION.RELEASE);
+    } else {
+      result.notImplemented();
+    }
+  }
+}
---
+++ ios/Classes/FlutterNativeUtilsPlugin.m
@@ -0,0 +1,20 @@
+#import "FlutterNativeUtilsPlugin.h"
+
+@implementation FlutterNativeUtilsPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"flutter_native_utils"
+            binaryMessenger:[registrar messenger]];
+  FlutterNativeUtilsPlugin* instance = [[FlutterNativeUtilsPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
